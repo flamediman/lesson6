05b7ba4b5fc2c51a0a75d157de3c6b83d654ac53
Anastasia Kultysheva <kasandrocka.ru@mail.ru>
2016-07-20 20:37:58 +0300

    new lesson6

--- main.c
+++ main.c
@@ -5,7 +5,14 @@
 char *END_OF_ARRAY = "FLAG";
 
 void show_menu() {
-  printf("1. Выделить память\n2. Заполнить ячейки данными\n3. Заменить элемент\n4. Вывести массив\n0. Выход\n");
+  printf(
+    "1. Выделить память\n"
+    "2. Заполнить ячейки данными\n"
+    "3. Заменить элемент\n"
+    "4. Перевернуть массив\n"
+    "5. Перевернуть строки\n"
+    "6. Вывести массив\n"
+    "0. Выход\n");
 }
 
 char **allocate_memory() {
@@ -48,9 +55,36 @@ void replace_memory(char **pointer) {
   scanf("%s", pointer[index - 1]);
 }
 
+int lenght_of_array(char **pointer) {
+  int i = 0;
+  while(strcmp(pointer[i], END_OF_ARRAY) != 0) {
+    i++;
+  }
+  return i - 1;
+}
+
+void reverse_array(char **pointer) {
+  int length = lenght_of_array(pointer);
+  int j = length;
+
+  for(int i = 0; i < length; i++, j--) {
+    char *mediator = malloc(strlen(pointer[i]) * sizeof(char));
+    mediator = pointer[i];
+    pointer[i] = pointer[j];
+    pointer[j] = mediator;
+    mediator = pointer[i];
+  }
+}
+
 int main() {
   int action;
   char **pointer;
+  pointer = malloc(5 * sizeof(char *));
+  pointer[0] = "asd";
+  pointer[1] = "123";
+  pointer[2] = "lald";
+  pointer[3] = "mnmn";
+  pointer[4] = END_OF_ARRAY;
 
   while(1) {
     show_menu();
@@ -69,6 +103,12 @@ int main() {
         replace_memory(pointer);
         break;
       case 4:
+        reverse_array(pointer);
+        break;
+      case 5:
+
+        break;
+      case 6:
         print_memory(pointer);
         break;
     }
